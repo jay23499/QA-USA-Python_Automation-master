26c26e15e83644bea90812bbc79dd8710ac80352
jay <jerrywilliams1177@gmail.com>
2025-09-22 15:23:01 -0400

    sprint 9/22/25

--- data.py
+++
@@ -1,7 +0,0 @@
-URBAN_ROUTES_URL = "https://cnt-4239d106-4877-4509-8a6c-fd1713293c72.containerhub.tripleten-services.com/"
-ADDRESS_FROM = "East 2nd Street, 601"
-ADDRESS_TO = "1300 1st St"
-PHONE_NUMBER = "+1 123 123 72 12"
-CARD_NUMBER = "1234 5678 9100 1234"
-CARD_CODE = "1111"
-MESSAGE_FOR_DRIVER = "Stop at the juice bar, please"

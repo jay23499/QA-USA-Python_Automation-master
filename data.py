c40c764863a827ac30846b9fc5ded7fc26e2d476
jay <jerrywilliams1177@gmail.com>
2025-10-01 23:04:34 -0500

    new

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = "https://cnt-e0232d52-2b6d-4049-a6d8-fbe1cc3de024.containerhub.tripleten-services.com//"
+URBAN_ROUTES_URL = "https://cnt-95cd3982-30aa-4d92-9890-80b22335d980.containerhub.tripleten-services.com/"
 ADDRESS_FROM = "East 2nd Street, 601"
 ADDRESS_TO = "1300 1st St"
 PHONE_NUMBER = "+1 123 123 72 12"
